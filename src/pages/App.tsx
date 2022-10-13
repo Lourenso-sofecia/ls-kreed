9269163a4bd83546dd40d30d753fb1e09a0ab345
sofecia <lourenso.sofecia@gmail.com>
2022-10-13 21:52:55 +0100

    iniciando o Projecto

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Frontend from '../templates/Frontend';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Frontend>
+      templates
+    </Frontend>
+  )
+}
+
+export default App
---
+++ src/templates/Frontend/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles';
+
+type FrontendProps = {
+    children: React.ReactNode;
+};
+
+const Frontend = ({children}: FrontendProps ) => {
+  return (
+    <S.Container>
+        Teste Frontend { children}
+    </S.Container>
+  );
+};
+
+export default Frontend;
