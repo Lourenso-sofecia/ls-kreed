46d1ce728a2ca55645156e4455388ad4b6a28f3c
sofecia <lourenso.sofecia@gmail.com>
2022-10-15 23:40:39 +0100

    Refactor code main

--- src/organisms/ProductStats/styles.ts
+++ src/organisms/ProductStats/styles.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
     position: relative;
     text-align: right;
+    width: 50%;
+    
     img {
         width: 387px;
         height: 450px;
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -3,20 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     flex-direction: row;
+
     > div {
         width: 50%;
     }
-    
-`;
-export const ColumnImage = styled.div`
-    position: relative;
-    text-align: right;
-    img {
-        width: 387px;
-        height: 450px;
-        border-radius: 40px;
-        
-        margin-left: 160px;
-    }
-    
 `;
